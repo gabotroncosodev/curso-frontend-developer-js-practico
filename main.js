8a4b4ed2964a8d022ebe5d4e3699a50934df991c
Gabriel Troncoso <gabotroncosodev@gmail.com>
2022-09-18 22:10:28 -0300

    se agrega menu izquierdo mobile

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamburgesaIcon = document.querySelector('.menu');
+const mobilemenu= document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburgesaIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+function toggleMobileMenu(){
+    mobilemenu.classList.toggle('inactive');
 }
